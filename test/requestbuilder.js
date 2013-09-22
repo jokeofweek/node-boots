4d91c75d428903b1fe1f9c70d133c20596f7b26f
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2013-09-22 09:02:35 -0400

    Added more tests for RequestBuilder.js

--- test/requestbuilder.js
+++ test/requestbuilder.js
@@ -9,7 +9,7 @@ var getReq = function(str) {
 	return RequestBuilder.RequestBuilder(new Buffer(str));
 };
 
-// TODO: More tests!
+// TODO: More tests! 
 
 module.exports = {
 	'testRequestBuilderIsExported': function(test) {
@@ -29,22 +29,48 @@ module.exports = {
 	},
 	'testCommandIsExtractedProperly': function(test) {
 		var tests = [
-			["CONNECT", "CONNECT\n\n\n\0"],
-			["CONNECT", "CONNECT\r\n\n\n\0"],
-			["CONNECT", "CONNECT\r\n\r\n\r\n\0"],
-			["CONNECT", "CONNECT\nk:v\nk:v\nk:v\n\n\0"],
-			["CONNECT", "CONNECT\nk:v\nk:v\nk:v\n\nSome body\\n\\n\0"],
-			["ANOTHERCOMMAND", "ANOTHERCOMMAND\n\n\n\0"],
-			["ANOTHERCOMMAND", "ANOTHERCOMMAND\r\n\n\n\0"],
-			["ANOTHERCOMMAND", "ANOTHERCOMMAND\r\n\r\n\r\n\0"],
-			["ANOTHERCOMMAND", "ANOTHERCOMMAND\nk:v\nk:v\nk:v\n\n\0"],
-			["ANOTHERCOMMAND", "ANOTHERCOMMAND\nk:v\nk:v\nk:v\n\nSome body\\n\\n\0"]
+			["CONNECT\n\n\n\0", "CONNECT"],
+			["CONNECT\r\n\n\n\0", "CONNECT"],
+			["CONNECT\r\n\r\n\r\n\0", "CONNECT"],
+			["CONNECT\nk:v\nk:v\nk:v\n\n\0", "CONNECT"],
+			["CONNECT\nk:v\nk:v\nk:v\n\nSome body\\n\\n\0", "CONNECT"],
+			["ANOTHERCOMMAND\n\n\n\0", "ANOTHERCOMMAND"],
+			["ANOTHERCOMMAND\r\n\n\n\0", "ANOTHERCOMMAND"],
+			["ANOTHERCOMMAND\r\n\r\n\r\n\0", "ANOTHERCOMMAND"],
+			["ANOTHERCOMMAND\nk:v\nk:v\nk:v\n\n\0", "ANOTHERCOMMAND"],
+			["ANOTHERCOMMAND\nk:v\nk:v\nk:v\n\nSome body\\n\\n\0", "ANOTHERCOMMAND"]
 		];
 
 		for (var i = 0; i < tests.length; i++) {
-			var req = getReq(tests[i][1]);
-			test.equals(req.getCommand(), tests[i][0], 
+			var req = getReq(tests[i][0]);
+			test.equals(req.getCommand(), tests[i][1], 
 				"Command could not be parsed correctly.");
+		}
+
+		test.done();
+	},
+	'testSimpleHeadersAreExtractedProperly': function(test) {
+		var tests = [
+			["CONNECT\n\n\n\0", {}],
+			["CONNECT\nk:\n\n\n\0", {k:''}],
+			["CONNECT\nk:\nk2:\n\n\0", {k:'', k2:''}],
+			["CONNECT\nk:v\n\n\n\0", {k:'v'}],
+			["CONNECT\nk:v\nk2:v2\n\n\0", {k:'v', k2: 'v2'}],
+			["CONNECT\nk:v\nk2:\nk3:v3\nk4:v4\nk5:\n\n\0", 
+				{k:'v', k2: '', k3: 'v3', k4: 'v4', k5: ''}],
+			["CONNECT\r\n\r\n\r\n\0", {}],
+			["CONNECT\r\nk:\r\n\r\n\r\n\0", {k:''}],
+			["CONNECT\r\nk:\r\nk2:\r\n\r\n\0", {k:'', k2:''}],
+			["CONNECT\r\nk:v\r\n\r\n\r\n\0", {k:'v'}],
+			["CONNECT\r\nk:v\r\nk2:v2\r\n\r\n\0", {k:'v', k2: 'v2'}],
+			["CONNECT\r\nk:v\r\nk2:\r\nk3:v3\r\nk4:v4\r\nk5:\r\n\r\n\0", 
+				{k:'v', k2: '', k3: 'v3', k4: 'v4', k5: ''}]
+		];
+
+		for (var i = 0; i < tests.length; i++) {
+			var req = getReq(tests[i][0]);
+			test.deepEqual(req.getHeaders(), tests[i][1], 
+				"Header could not be parsed correctly.");
 		}
 
 		test.done();
